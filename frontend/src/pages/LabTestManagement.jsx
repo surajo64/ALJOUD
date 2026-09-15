b27e4cebb3e73fcf113db30aeeca6dcebdb41b41
surajo64 <surajoumar52@gmail.com>
2026-09-15 15:57:58 +0100

    Urine culture fix

--- frontend/src/pages/LabTestManagement.jsx
+++ frontend/src/pages/LabTestManagement.jsx
@@ -393,7 +393,7 @@ NORMAL RANGES:
 RESULT:
 - Malaria: _____ (Normal: Negative)
 `,
-        urineCulture: `URINE CULTURE
+        urineCulture: `CULTURE
 
 PHYSICAL EXAMINATION:
 - Color: _____ (Normal: Yellow to amber)
